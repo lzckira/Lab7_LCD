e8f616d1f565dd3ce4c1842e8b57f50799e2bdaf
lzckira <43661779+lzckira@users.noreply.github.com>
2019-07-08 18:39:24 -0700

    Update io.c

--- source/io.c
+++ source/io.c
@@ -1,4 +1,4 @@
-nclude <avr/io.h>
+#include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdio.h>
 #include "io.h"
@@ -22,59 +22,59 @@ void LCD_ClearScreen(void) {
 
 void LCD_init(void) {
 
-    wait for 100 ms.
-    	delay_ms(100);
-    		LCD_WriteCommand(0x38);
-    			LCD_WriteCommand(0x06);
-    				LCD_WriteCommand(0x0f);
-    					LCD_WriteCommand(0x01);
-    						delay_ms(10);						 
-    						}
-    
-    						void LCD_WriteCommand (unsigned char Command) {
-    						   CLR_BIT(CONTROL_BUS,RS);
-    						      DATA_BUS = Command;
-    						         SET_BIT(CONTROL_BUS,E);
-    						            asm("nop");
-    						               CLR_BIT(CONTROL_BUS,E);
-    						                  delay_ms(2); // ClearScreen requires 1.52ms to execute
-    						                  }
-    
-    						                  void LCD_WriteData(unsigned char Data) {
-    						                     SET_BIT(CONTROL_BUS,RS);
-    						                        DATA_BUS = Data;
-    						                           SET_BIT(CONTROL_BUS,E);
-    						                              asm("nop");
-    						                                 CLR_BIT(CONTROL_BUS,E);
-    						                                    delay_ms(1);
-    						                                    }
-    
-    						                                    void LCD_DisplayString( unsigned char column, const unsigned char* string) {
-    						                                       LCD_ClearScreen();
-    						                                          unsigned char c = column;
-    						                                             while(*string) {
-    						                                                   LCD_Cursor(c++);
-    						                                                         LCD_WriteData(*string++);
-    						                                                            }
-    						                                                            }
-    
-    						                                                            void LCD_Cursor(unsigned char column) {
-    //						                                                               if ( column < 17 ) { // 16x1 LCD: column < 9
-    //						                                                               						// 16x2 LCD: column < 17
-    //						                                                               						      LCD_WriteCommand(0x80 + column - 1);
-    //						                                                               						         } else {
-    //						                                                               						               LCD_WriteCommand(0xB8 + column - 9);	// 16x1 LCD: column - 1
-    //						                                                               						               											// 16x2 LCD: column - 9
-    //						                                                               						               											   }
-    //						                                                               						               											   }
-    //
-    //						                                                               						               											   void delay_ms(int miliSec) //for 8 Mhz crystal
-    //
-    //						                                                               						               											   {
-    //						                                                               						               											       int i,j;
-    //						                                                               						               											           for(i=0;i<miliSec;i++)
-    //						                                                               						               											               for(j=0;j<775;j++)
-    //						                                                               						               											                 {
-    //						                                                               						               											                    asm("nop");
-    //						                                                               						               											                      }
-    //						                                                               						               											                      }
+    //wait for 100 ms.
+	delay_ms(100);
+	LCD_WriteCommand(0x38);
+	LCD_WriteCommand(0x06);
+	LCD_WriteCommand(0x0f);
+	LCD_WriteCommand(0x01);
+	delay_ms(10);						 
+}
+
+void LCD_WriteCommand (unsigned char Command) {
+   CLR_BIT(CONTROL_BUS,RS);
+   DATA_BUS = Command;
+   SET_BIT(CONTROL_BUS,E);
+   asm("nop");
+   CLR_BIT(CONTROL_BUS,E);
+   delay_ms(2); // ClearScreen requires 1.52ms to execute
+}
+
+void LCD_WriteData(unsigned char Data) {
+   SET_BIT(CONTROL_BUS,RS);
+   DATA_BUS = Data;
+   SET_BIT(CONTROL_BUS,E);
+   asm("nop");
+   CLR_BIT(CONTROL_BUS,E);
+   delay_ms(1);
+}
+
+void LCD_DisplayString( unsigned char column, const unsigned char* string) {
+   LCD_ClearScreen();
+   unsigned char c = column;
+   while(*string) {
+      LCD_Cursor(c++);
+      LCD_WriteData(*string++);
+   }
+}
+
+void LCD_Cursor(unsigned char column) {
+   if ( column < 17 ) { // 16x1 LCD: column < 9
+						// 16x2 LCD: column < 17
+      LCD_WriteCommand(0x80 + column - 1);
+   } else {
+      LCD_WriteCommand(0xB8 + column - 9);	// 16x1 LCD: column - 1
+											// 16x2 LCD: column - 9
+   }
+}
+
+void delay_ms(int miliSec) //for 8 Mhz crystal
+
+{
+    int i,j;
+    for(i=0;i<miliSec;i++)
+    for(j=0;j<775;j++)
+  {
+   asm("nop");
+  }
+}
